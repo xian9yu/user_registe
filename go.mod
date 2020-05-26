0d2b3782ee06d6f9db550fc0c29d6e2383adcd44
Stray Rabbit <56214042+etinifninfinite@users.noreply.github.com>
2020-05-26 11:29:10 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,11 @@
+module democ
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gomodule/redigo v1.8.1
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	xorm.io/xorm v1.0.1
+)
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"democ/ctrls"
+	"democ/models"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter(router *gin.Engine) {
+	router.GET("/", ctrls.Index)
+	router.GET("/user/:id", ctrls.GetOneUser)
+	router.GET("/register", ctrls.Register)
+	router.GET("/auth", ctrls.CheckAuthLink)
+	router.POST("/register", ctrls.Register)
+	//router.POST("/register/:on_click", ctrls.Register)
+	router.POST("/login", ctrls.Login)
+
+}
+
+func main() {
+	//初始化sql
+	models.InitSQL()
+
+	//在控制台打印 sql信息，便于调试
+	models.DB.ShowSQL(true)
+
+	//初始化router
+	r := gin.Default()
+	initRouter(r)
+
+	//加载views目录下的所有html模板文件
+	r.LoadHTMLGlob("./views/*")
+
+	//使用gin自带的异常恢复中间件，避免出现异常时程序退出
+	r.Use(gin.Recovery())
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
