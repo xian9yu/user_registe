79df8d5cd1cc02ca3639dd73be77f376a769f5a1
Stray Rabbit <56214042+etinifninfinite@users.noreply.github.com>
2020-06-24 09:47:53 +0800

    update send mail

--- ctrls/sendmail.go
+++ ctrls/sendmail.go
@@ -27,14 +27,14 @@ func sendMail(code string, sjr string) {
 	//126邮箱授权码
 	//BDXZQHHCQUMTJPTC
 	m := gomail.NewMessage()
-	m.SetAddressHeader("From", "etinifninfinite@126.com", "注册验证邮件") // 发件人
+	m.SetAddressHeader("From", "etinifninfinite@gmail.com", "注册验证邮件") // 发件人
 	m.SetHeader("To", m.FormatAddress(sjr, "111"))                  //收件人
 	m.SetHeader("Subject", "网站验证激活邮件")                              // 主题
 	body := "点击链接即可激活账号，1小时内有效，请尽快完成激活：" + "http://localhost:8080/auth?email=" + sjr + "&" + "code=" + code
 	//"<a href='http://localhost:8080/auth/?email=" + sjr + "&" + "code=" + code + ">点击激活</a><br>"
 	m.SetBody("text/html", body) // 正文
 
-	d := gomail.NewDialer("smtp.126.com", 465, "etinifninfinite@126.com", "BDXZQHHCQUMTJPTC") // 发送邮件服务器、端口、发件人账号、发件人密码
+	d := gomail.NewDialer("smtp.126.com", 465, "etinifninfinite@gmail.com", "HHCZQTJPTQUMBDXC") // 发送邮件服务器、端口、发件人账号、发件人密码
 
 	err := d.DialAndSend(m)
 	if err != nil {
